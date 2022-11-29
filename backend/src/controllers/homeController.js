ae6d9ad1237883df5cd52636215b464c9d11bbf9
TQTung <trinhquangtung99@gmail.com>
2022-11-29 11:11:31 +0700

    config table database

--- backend/src/controllers/homeController.js
+++ backend/src/controllers/homeController.js
@@ -1,5 +1,12 @@
-const getHomePage = (req, res) => {
-  return res.render("homePage.ejs");
+import db from "../models/index";
+
+const getHomePage = async (req, res) => {
+  try {
+    const data = await db.User.findAll();
+    return res.render("homePage.ejs", { dataUser: JSON.stringify(data) });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { getHomePage };
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
 require("dotenv").config();
 
 const app = express();
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRoutes(app);
+
+// connect to database
+connectDB();
 
 // run server
 const port = process.env.PORT;
